d17454a8e1ec7028d6f7ffa9774b1306977bebf1
den59k <den59k@gmail.com>
2024-04-14 17:15:57 +0500

    v1.1.1

--- src/bin.ts
+++ src/bin.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { resolve } from "path";
 import { generateDoc } from "./generateDoc/generateDoc";
 import ts from 'typescript'
@@ -18,4 +20,6 @@ if (process.argv[2] === "generate") {
   const outputFile = process.argv[4] ?? "linkmeup.d.ts"
   fs.writeFileSync(resolve(outputFile), data)
   console.info(`File ${outputFile} created!`)
+} else {
+  console.info("Use linkmeup generate <sourceDir> <targetFile> to create linkmeup d.ts file")
 }
